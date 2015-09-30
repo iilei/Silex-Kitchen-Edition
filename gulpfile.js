50cce766e8cf513322b5edee9f0c063af23bee81
Jochen Preusche <jochen.preusche@xing.com>
2015-09-30 22:24:32 +0200

    WIP - scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp      = require('gulp'),
-    util      = require('gulp-util'),
+'use strict';
+
+let gulp      = require('gulp'),
     hash      = require('gulp-hash'),
     uglify    = require('gulp-uglify'),
     concat    = require('gulp-concat'),
@@ -15,14 +16,16 @@ var gulp      = require('gulp'),
     },
     destinations = {
       js: 'web/assets/js',
-      images: 'web/assets/img'
+      images: 'web/assets/img',
+      styles: 'web/assets/styles'
     };
 
 gulp.task('clean', function () {
     return del([
       'web/assets/js/**/*.js',
       'web/assets/js/**/*.js.map',
-      'web/assets/img/**/*'
+      'web/assets/img/**/*',
+      RES_DIR + '/rev-manifest.json'
     ]);
 });
 
@@ -35,11 +38,26 @@ gulp.task('scripts', function(done) {
       .pipe(hash({template: "<%= name %>.<%= hash %>.min<%= ext %>"})) // Add hashes to the files' name(s)
       .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('web/assets')) // Write the renamed files
-      .pipe(hash.manifest('rev-manifest.json')) // Switch to the manifest file
+      .pipe(hash.manifest('rev-manifest.json', true)) // Switch to the manifest file
       .pipe(gulp.dest(RES_DIR)); // Write the manifest file
 
   done();
 });
+
+//styles
+// gulp.task('styles', function() {
+// 	return gulp.src([RES_DIR])
+// 		.pipe(plumber(plumberErrorHandler))
+// 		.pipe(compass({
+// 			// css: 'html/css',
+// 			sass: '/sass/**/*.scss'
+// 			//, image: 'html/images'
+// 		}))
+// 		.pipe(gulp.dest('html/css'))
+// 		.pipe(rename({ suffix: '.min' }))
+// 		.pipe(minifycss())
+// 		.pipe(gulp.dest('html/css'));
+// });
 
 // Copy all static images
 gulp.task('images', function() {
@@ -54,7 +72,11 @@ gulp.task('images', function() {
 //   gulp.watch(resources.images, ['images']);
 // });
 
-gulp.task('default', gulp.series('clean', 'scripts', 'images', function(done) {
-  // do more stuff
-  done();
-}));
+gulp.task('default',
+  gulp.series('clean',
+    gulp.parallel('scripts', /* 'styles', */ 'images'),
+    function (done) {
+      done();
+    }
+  )
+);
